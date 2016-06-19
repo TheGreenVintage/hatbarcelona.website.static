b13fe5c727cdcdd6dd1e3c6417d4404644f9945e
Ferran Basora <fcsonline@gmail.com>
2016-06-19 23:58:56 +0200

    Private section

--- _tools/downloadi18n.sh
+++ _tools/downloadi18n.sh
@@ -4,4 +4,4 @@ FILE="/tmp/hatbarcelona.i18n.csv"
 echo "Downloading i18n CSV file..."
 rm -f ${FILE}
 wget -q -O ${FILE} "https://docs.google.com/spreadsheets/d/1ffD7Ef8Kp_lnAokk3Pc-s4CKpZUJDLSod4YxXW9UIlA/pub?gid=0&single=true&output=csv"
-ruby ../_tools/csv2yaml.rb ${FILE}
+ruby2.3 ../_tools/csv2yaml.rb ${FILE}
